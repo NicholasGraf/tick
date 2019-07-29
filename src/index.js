e9cf8a82b42dd490963aa9742f1e11ebd2c78595
nicholasgraf <ngraf@1stquad.com>
2019-07-29 09:35:24 -0500

    Options update and ticker icon creation.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,66 @@
 // DO NOT EDIT
-var $ = require("jquery");
-require("./index.scss");
+var $ = require('jquery');
+require('./index.scss');
 // End: DO NOT EDIT
 
 
 // Custom Javascript Below Here
+
+
+var list = [];
+var max = 20;
+var min = 0;
+
+for (var i = 1; i < 50; i++) {
+    list.push({
+        "id": i,
+        "name": "Location " + i,
+        "city1": Math.floor(Math.random() * (max - min + 1) + min),
+        "city2": Math.floor(Math.random() * (max - min + 1) + min),
+        "city3": Math.floor(Math.random() * (max - min + 1) + min),
+        "city4": Math.floor(Math.random() * (max - min + 1) + min),
+        "city5": Math.floor(Math.random() * (max - min + 1) + min)
+    });
+}
+
+// console.table(list);
+
+
+var items = [];
+var options = [];
+
+for (var l in list) {
+    items.push('<div class="item item-' + list[l].id + '">' +
+        '   <div>' + list[l].name + '</div>' +
+        '   <div>' + list[l].city1 + '</div>' +
+        '   <div>' + list[l].city2 + '</div>' +
+        '   <div>' + list[l].city3 + '</div>' +
+        '   <div>' + list[l].city4 + '</div>' +
+        '   <div>' + list[l].city5 + '</div>' +
+        '</div>');
+    options.push('<div><input type="checkbox" name="item-' + list[l].id + '" checked /> <label for="item-' + list[l].id + '">' + list[l].name + '</label></div>');
+}
+
+//console.table(items);
+
+$('.list').html(items);
+$('.options').html(options);
+
+
+$(document).on('click', '.options input', function (e) {
+    var name = $(e.target).attr("name");
+    var target = $('.list .' + name);
+    if ($(this).is(':checked')) {
+        target.show();
+    } else {
+
+        target.hide();
+    }
+})
+
+
+$(document).on('click', '.open-options', function () {
+    $('.page-options').toggleClass('toggle');
+});
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry: "./src/index.js",
     output: {
         path: path.resolve(__dirname, 'dist'), // output directory
-       // filename: "[name].js" // name of the generated bundle
+        // filename: "[name].js" // name of the generated bundle
         filename: "index.js" // name of the generated bundle
     },
     module: {
@@ -14,6 +15,10 @@ module.exports = {
             {
                 test: /\.scss$/,
                 loader: ["style-loader", "css-loader?sourceMap", "sass-loader?sourceMap"]
+            },
+            {
+                test: /\.(jpe?g|gif|png|svg|woff|ttf|wav|mp3)$/,
+                loader: "file"
             }
         ]
     },
@@ -21,6 +26,9 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: "src/index.html",
             inject: "body"
-        })
+        }),
+        new CopyPlugin([
+            { from: 'src/img/', to: 'img/' }
+        ])
     ]
 };
