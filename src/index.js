62ec5697e302c0e952e17e1022c0cc39d357d987
nicholasgraf <ngraf@1stquad.com>
2019-07-31 17:32:33 -0500

    Account + details + options fix.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // DO NOT EDIT
-var $ = require('jquery');
+const $ = require('jquery');
 require('select2/dist/css/select2.min.css');
 require('select2/dist/js/select2.min.js');
 require('./index.scss');
@@ -8,74 +8,106 @@ require('./index.scss');
 
 // Custom Javascript Below Here
 
+// Variables
+let list = [];
+let select = [];
+let selected = [];
+let items = [];
+let locations;
+let size;
+let user;
 
-var list = [];
-var max = 20;
-var min = 0;
-
-for (var i = 1; i < 50; i++) {
-    list.push({
-        "id": i,
-        "name": "Location " + i,
-        "city1": Math.floor(Math.random() * (max - min + 1) + min),
-        "city2": Math.floor(Math.random() * (max - min + 1) + min),
-        "city3": Math.floor(Math.random() * (max - min + 1) + min),
-        "city4": Math.floor(Math.random() * (max - min + 1) + min),
-        "city5": Math.floor(Math.random() * (max - min + 1) + min)
-    });
-}
-
-// console.table(list);
-
-
-var items = [];
-var options = [];
-
-
-
-for (var l in list) {
-    items.push('<div class="item item-' + list[l].id + '">' +
-        '   <div>' + list[l].name + '</div>' +
-        '   <div>' + list[l].city1 + '</div>' +
-        '   <div>' + list[l].city2 + '</div>' +
-        '   <div>' + list[l].city3 + '</div>' +
-        '   <div>' + list[l].city4 + '</div>' +
-        '   <div>' + list[l].city5 + '</div>' +
-        '</div>');
-    options.push('<option value="item-' + list[l].id + '">' + list[l].name + '</option>');
-}
-
-$(document).ready(function () {
-    $('.options').select2({
-        placeholder: 'Select a Location',
-        closeOnSelect: false
-    });
-});
-
-
-
-//console.table(items);
-
-$('.list-results').html(items);
-$('.options').html(options);
-
-
-$(document).on('change', '.options', function () {
-    var data = $(this).select2('data');
+// Format Tabel Data
+function listCheck() {
+    var data = $('.options').select2('data');
     $('.list .item').hide();
     for (var d in data) {
         var name = data[d].id;
-        var target = $('.list .' + name);
-        target.css('display','flex');
+        var target = $('.list .item-' + name);
+        target.css('display', 'flex');
     }
-});
+}
 
+// Get Table Data
+$.ajax({
+    url: 'data.json',
+    dataType: 'json',
+    success: function (data) {
+        locations = data.locations;
+        user = data.user;
+        selected = user.selected;
+        size = user.size;
+
+        for (var l in locations) {
+            var loc = locations[l];
+            list.push(loc);
+            select.push({ "id": loc.id, "text": loc.name });
+        }
+
+    }
+}).done(function () {
+
+    // Load Items
+    for (var l in list) {
+        var item = list[l];
+        var r = item.results;
+        items.push(
+            '<div class="item item-' + item.id + '">' +
+            '   <div>' + item.name + '</div>' +
+            '   <div>' + r.result1 + '</div>' +
+            '   <div>' + r.result2 + '</div>' +
+            '   <div>' + r.result3 + '</div>' +
+            '   <div>' + r.result4 + '</div>' +
+            '   <div>' + r.result5 + '</div>' +
+            '</div>');
+    }
+    $('.list-results').html(items);
+
+    // Init Select2
+    $(document).ready(function () {
+        $('.options').select2({
+            placeholder: 'Select a Location',
+            closeOnSelect: false,
+            data: select
+        }).val(selected).trigger('change');
+
+        listCheck();
+    });
+
+    // Init Size 
+    if (size == 3) {
+        $('.list-results').addClass('large');
+    } else if (size == 3) {
+        $('.list-results').addClass('medium');
+    }
+})
+
+
+// Options
+$(document).on('change', '.options', function () {
+    listCheck();
+});
 
 $(document).on('click', '.open-options, .close-options', function () {
     $('.page-options').toggleClass('toggle');
 });
 
+$(document).on('click', function (e) {
+    if (
+        $(e.target).closest('.page-options').length == 0
+        && $(e.target) != $('.open-options')
+        && $(e.target).attr('class') != 'select2-selection__choice__remove'
+        && $(e.target).closest('.open-options').length == 0
+        && $(e.target).closest('.select2-container').length == 0
+    ) {
+        if ($('.page-options').hasClass('toggle')) {
+            $('.page-options').removeClass('toggle');
+        }
+    }
+});
 
+
+// Table Font Size
 $(document).on('click', '.list-controls .small', function () {
     $('.list-results').removeClass('medium large');
 });
@@ -88,4 +120,40 @@ $(document).on('click', '.list-controls .medium', function () {
 $(document).on('click', '.list-controls .large', function () {
     $('.list-results').removeClass('medium');
     $('.list-results').addClass('large');
+});
+
+
+// Item Details
+$(document).on('click', '.item', function () {
+    $('.item-details').addClass('toggle');
+    let details = $(this).html();
+    $('.item-details').html(details);
+});
+
+$(document).on('click', function (e) {
+    if ($(e.target).closest('.item-details').length == 0
+        && $(e.target).closest('.item').length == 0) {
+        $('.item-details').removeClass('toggle');
+    }
+});
+
+
+// Account Details
+$(document).on('click', '.account', function () {
+    var account = $('<div/>', {
+        class: 'account-details',
+    });
+
+    account.appendTo('.header .controls');
+    account.append('<h1>Account</h1>' +
+        '<p><b>Username:</b> ' + user.name + '</p>' +
+        '<p><b>Email:</b> ' + user.email + '</p>'
+    );
+});
+
+$(document).on('click', function (e) {
+    if ($(e.target).closest('.account-details').length == 0
+        && $(e.target).closest('.account').length == 0) {
+        $('.account-details').remove();
+    }
 });
--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,13 @@ const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+var __dirname = path.resolve();
+
 module.exports = {
     mode: 'development',
     entry: "./src/index.js",
     output: {
-        path: path.resolve(__dirname, 'dist'), // output directory
+        path: path.resolve(__dirname, 'dist'),
         // filename: "[name].js" // name of the generated bundle
         filename: "index.js" // name of the generated bundle
     },
@@ -32,7 +34,8 @@ module.exports = {
             inject: "body"
         }),
         new CopyPlugin([
-            { from: 'src/img/', to: 'img/' }
+            { from: 'src/img/', to: 'img/' },
+            { from: "src/data.json", to: '' }
         ])
     ]
 };
